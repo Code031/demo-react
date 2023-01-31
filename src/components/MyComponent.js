984659c782ec2aeef2da21ea5cb361272fc6984b
Vu Cong Anh <conganhyh5411@gmail.com>
2023-01-31 14:42:47 +0700

    state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Anh',
+        address: 'Hà Nội',
+        age: 22
+    }
     //JSX
     render() {
         return (
             <div>
-                my first component {Math.random()}
+                Tên của tôi là {this.state.name} và tôi sống ở {this.state.address}
             </div>
         );
     }
