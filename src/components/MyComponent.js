47e106430b089b0d6b91a3240c0a2e6500d265a9
Vu Cong Anh <conganhyh5411@gmail.com>
2023-02-01 11:14:17 +0700

    setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,20 +8,26 @@ class MyComponent extends React.Component {
     }
 
     handelClick(event) {
-        console.log(event)
+        // console.log('Click');
+        // console.log('Tên của tôi là', this.state.name)
+
+        this.setState({
+            name: 'Công Anh',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
     }
 
     handelOnMouseOver(event) {
-        console.log(event)
+        //console.log(event)
     }
 
     //JSX
     render() {
         return (
             <div>
-                Tên của tôi là {this.state.name} và tôi sống ở {this.state.address}
+                Tên của tôi là {this.state.name} số ngẫu nhiên {this.state.age}
                 <button onMouseOver={this.handelOnMouseOver}>Hover</button>
-                <button onClick={this.handelClick}>Click</button>
+                <button onClick={(event) => { this.handelClick(event) }}>Click</button>
             </div>
         );
     }
