995b276987706a0d86b01ef77fcd1b2e52af1f3f
Vu Cong Anh <conganhyh5411@gmail.com>
2023-02-01 10:01:40 +0700

    dom events

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,11 +6,22 @@ class MyComponent extends React.Component {
         address: 'Hà Nội',
         age: 22
     }
+
+    handelClick(event) {
+        console.log(event)
+    }
+
+    handelOnMouseOver(event) {
+        console.log(event)
+    }
+
     //JSX
     render() {
         return (
             <div>
                 Tên của tôi là {this.state.name} và tôi sống ở {this.state.address}
+                <button onMouseOver={this.handelOnMouseOver}>Hover</button>
+                <button onClick={this.handelClick}>Click</button>
             </div>
         );
     }
