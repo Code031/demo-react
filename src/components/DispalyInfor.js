d70f9d07184b9036479ea07635f64e6bb4d3c9a8
Vu Cong Anh <conganhyh5411@gmail.com>
2023-03-28 21:10:04 +0700

    stateful stateless

--- src/components/DispalyInfor.js
+++ src/components/DispalyInfor.js
@@ -2,47 +2,60 @@ import React from "react";
 import './DisplayInfor.scss';
 import logo from "./../logo.svg";
 
-class DisplayIinfo extends React.Component {
-    state = {
-        isShowListUser: true
-    }
+// class DisplayIinfo extends React.Component {
 
-    handelShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
-    render() {
-        const { listUsers } = this.props
-        // props => viết tắt của properties
-        return (
-            <div className='display-infor-container'>
-                {/* <img src={logo} /> */}
-                <div>
-                    <span onClick={() => { this.handelShowHide() }}>
-                        {this.state.isShowListUser === true ? "Ẩn danh sách người dùng : " : "Hiện danh sách người dùng"}
-                    </span>
-                </div>
-                {this.state.isShowListUser &&
-                    <>
-                        {listUsers.map((user, index) => {
-                            return (
-                                <div key={user.id} className={+user.age > 18 ? "red" : "green"}>
-                                    <div >Tên tôi là {user.name}</div>
-                                    <div>Tôi {user.age} tuổi</div>
-                                    <div>
-                                        <button onClick={() => this.props.handleDeleteUser(user.id)}> Xoá </button>
-                                    </div>
-                                    <hr></hr>
+//     render() {
+//         const { listUsers } = this.props
+//         // props => viết tắt của properties
+//         return (
+//             <div className='display-infor-container'>
+
+//                 {true &&
+//                     <>
+//                         {listUsers.map((user, index) => {
+//                             return (
+//                                 <div key={user.id} className={+user.age > 18 ? "red" : "green"}>
+//                                     <div >Tên tôi là {user.name}</div>
+//                                     <div>Tôi {user.age} tuổi</div>
+//                                     <div>
+//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}> Xoá </button>
+//                                     </div>
+//                                     <hr></hr>
+//                                 </div>
+//                             )
+
+//                         })}
+//                     </>
+//                 }
+//             </div>
+//         )
+//     }
+// }
+const DisplayIinfo = (props) => {
+    const { listUsers } = props
+    // props => viết tắt của properties
+    return (
+        <div className='display-infor-container'>
+
+            {true &&
+                <>
+                    {listUsers.map((user, index) => {
+                        return (
+                            <div key={user.id} className={+user.age > 18 ? "red" : "green"}>
+                                <div >Tên tôi là {user.name}</div>
+                                <div>Tôi {user.age} tuổi</div>
+                                <div>
+                                    <button onClick={() => props.handleDeleteUser(user.id)}> Xoá </button>
                                 </div>
-                            )
+                                <hr></hr>
+                            </div>
+                        )
 
-                        })}
-                    </>
-                }
-            </div>
-        )
-    }
+                    })}
+                </>
+            }
+        </div>
+    )
+
 }
-
 export default DisplayIinfo;
